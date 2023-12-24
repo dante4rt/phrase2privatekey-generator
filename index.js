ecb0096da3eab93ec6bbd93402d57b3e62970f70
dante4rt <rxmxdhxni@gmail.com>
2023-12-25 00:16:02 +0800

    refactor: add commands on main func

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ import readlineSync from "readline-sync";
 import { ethers } from 'ethers';
 import { appendFileSync } from "fs";
 
+// Function to check if a mnemonic phrase is valid
 const isValidMnemonic = (phrase) => {
     return phrase.trim().split(" ").length >= 12 && phrase.trim().split(" ").length <= 24;
 };
 
+// Main function using async IIFE (Immediately Invoked Function Expression)
 (async () => {
     try {
         let numberOfPhrases;
+
+        // Get the number of phrases to convert from user input
         do {
             numberOfPhrases = parseInt(readlineSync.question(chalk.yellow("Enter the number of phrases to convert: ")), 10);
             if (isNaN(numberOfPhrases) || numberOfPhrases <= 0) {
@@ -19,13 +23,16 @@ const isValidMnemonic = (phrase) => {
 
         const phrases = [];
 
+        // Get each phrase from the user
         for (let i = 1; i <= numberOfPhrases; i++) {
             let phraseKey;
             do {
+                // Get user input for the phrase or mnemonic key
                 phraseKey = readlineSync.question(
                     chalk.yellow(`Input phrase or mnemonic key ${i}/${numberOfPhrases} (12 or 24 words is possible): `)
                 );
 
+                // Validate the entered phrase
                 if (!isValidMnemonic(phraseKey)) {
                     console.log(chalk.red(`Error for phrase ${i}/${numberOfPhrases}: Your phrase must have between 12 and 24 words and should not be empty. Please re-enter.`));
                 }
@@ -34,9 +41,11 @@ const isValidMnemonic = (phrase) => {
             phrases.push(phraseKey);
         }
 
+        // Convert each phrase to a private key and write to result.txt
         for (let i = 0; i < numberOfPhrases; i++) {
             const result = ethers.Wallet.fromPhrase(phrases[i]).privateKey;
             if (result) {
+                // Append the result to the result.txt file
                 appendFileSync('./result.txt', `Mnemonic: ${phrases[i]} || Private key: ${result}\n`);
             }
             console.log(chalk.green(`Conversion ${i + 1}/${numberOfPhrases} successful! Private key: ${result}`));
